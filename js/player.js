9bf76efc9f20006da1392ac920fe8ef99529ff06
tauhid880 <tauhid880@gmail.com>
2022-08-23 20:09:07 +0600

    project done

--- js/player.js
+++ js/player.js
@@ -27,3 +27,150 @@ function addPlayer(name, this_btn) {
     alert("You Can't Add More than 5 Players");
   }
 }
+
+// click = calculate-button
+
+function calculate() {
+  const length = orderList.children.length;
+
+  const perPlayer = getIDValue("per-player");
+  const perPlayerValue = makeFloat(perPlayer);
+
+  if (isNaN(perPlayerValue)) {
+    alert("Enter Amount for Per-Player");
+
+    setTextValue("player-expenses", "Amount");
+
+    setInputValue("per-player", "");
+    
+    setTextValue("total", "Amount");
+
+    // setInputValue("manager", '');
+    // setInputValue("coach", '');
+  } else if (perPlayerValue == 0) {
+    alert(`Player-Cost => (${perPlayerValue}) Amount can't be Zero`);
+
+    setTextValue("player-expenses", "Amount");
+
+    setInputValue("per-player", "");
+
+    setTextValue("total", "Amount");
+
+    // setInputValue("manager", '');
+    // setInputValue("coach", '');
+  } else if (perPlayerValue < 0) {
+    alert(`Player-Cost => (${perPlayerValue}) Amount can't be Negative`);
+
+    setTextValue("player-expenses", "Amount");
+
+    setInputValue("per-player", "");
+
+    setTextValue("total", "Amount");
+
+    // setInputValue("manager", '');
+    // setInputValue("coach", '');
+  } else if (length == 0) {
+    alert("Please Select some Players");
+
+    setTextValue("player-expenses", "Amount");
+
+    setInputValue("per-player", "");
+
+    setTextValue("total", "Amount");
+
+    // setInputValue("manager", '');
+    // setInputValue("coach", '');
+  } else {
+    const expenses = length * perPlayerValue;
+
+    setTextValue("player-expenses", expenses);
+
+    // setInputValue("per-player", '');
+
+    setTextValue("total", "Amount");
+
+    // setInputValue("manager", '');
+    // setInputValue("coach", '');
+  }
+}
+
+// click = calculate-total-button
+
+const calculateTotal = document
+  .getElementById("calculate-total")
+  .addEventListener("click", function () {
+    const playerExpenses = getIDInnerText("player-expenses");
+    const playerExpensesValue = makeFloat(playerExpenses);
+
+    const manager = getIDValue("manager");
+    const managerValue = makeFloat(manager);
+
+    const coach = getIDValue("coach");
+    const coachValue = makeFloat(coach);
+
+    if (isNaN(managerValue) || isNaN(coachValue)) {
+      alert("Enter Amount for Manager & Coach");
+
+      setTextValue("total", "Amount");
+
+      setInputValue("per-player", "");
+
+      // setInputValue("manager", '');
+      // setInputValue("coach", '');
+    } else if (managerValue == 0) {
+      alert(`Manager-Cost => (${managerValue}) Amount can't be Zero`);
+
+      setTextValue("total", "Amount");
+
+      setInputValue("per-player", "");
+
+      setInputValue("manager", "");
+      // setInputValue("coach", '');
+    } else if (coachValue == 0) {
+      alert(`Coach-Cost => (${coachValue}) Amount can't be Zero`);
+
+      setTextValue("total", "Amount");
+
+      setInputValue("per-player", "");
+
+      // setInputValue("manager", '');
+      setInputValue("coach", "");
+    } else if (managerValue < 0) {
+      alert(`Manager-Cost => (${managerValue}) Amount can't be Negative`);
+
+      setTextValue("total", "Amount");
+
+      setInputValue("per-player", "");
+
+      setInputValue("manager", "");
+      // setInputValue("coach", '');
+    } else if (coachValue < 0) {
+      alert(`Coach-Cost => (${coachValue}) Amount can't be Negative`);
+
+      setTextValue("total", "Amount");
+
+      setInputValue("per-player", "");
+
+      // setInputValue("manager", '');
+      setInputValue("coach", "");
+    }
+
+    
+    else if (playerExpenses == "Amount") {
+      alert(
+        "Please Calculate Player Expenses, by Selecting Some Players & Press 'Calculate' Button"
+      );
+
+      setTextValue("total", "Amount");
+
+      setInputValue("per-player", "");
+
+      // setInputValue("manager", '');
+      // setInputValue("coach", '');
+    } else {
+      const finalTotal = playerExpensesValue + managerValue + coachValue;
+      setTextValue("total", finalTotal);
+
+      setInputValue("per-player", "");
+    }
+  });
--- js/utilities.js
+++ js/utilities.js
@@ -17,3 +17,15 @@ function makeFloat(str) {
   const float = parseFloat(parseFloat(str).toFixed(2));
   return float;
 }
+
+// setTextValue
+function setTextValue(id, newValue) {
+  const textElementId = document.getElementById(id);
+  textElementId.innerText = newValue;
+}
+
+// setInputValue (should be into Empty)
+function setInputValue(id, empty) {
+  const inputId = document.getElementById(id);
+  inputId.value = empty;
+}
